78c7368258b6d7939b2f607cd267038145ab3e23
Samuel <samuelpl0888@gmail.com>
2023-02-14 15:24:07 -0400

    Add instance method to create JWT token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,10 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ name: user.name, token });
 };
 
 const login = async (req, res) => {
