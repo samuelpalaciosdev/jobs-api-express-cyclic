2bd061d6dcd7a8ff6a8827e444f62b1bb491d26d
Samuel <samuelpl0888@gmail.com>
2023-02-15 01:06:57 -0400

    Add delete job controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -51,7 +51,16 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.status(200).send('Job deleted');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOneAndDelete({ _id: jobId, createdBy: userId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
